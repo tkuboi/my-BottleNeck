06cc212be1a267ee70797711532ddc141d9bf28a
Toshihiro Kuboi <toshikuboi@hotmail.com>
2020-05-25 16:52:01 -0700

    save each np array instead

--- app/net/sort_dataset3.py
+++ app/net/sort_dataset3.py
@@ -7,9 +7,10 @@ from PIL import Image
 
 DIMENSION = (240, 320)
 
-def import_images(filepath, dim):
+def import_images(filepath, dim, out_dir):
     dir_path = "/".join(filepath.split("/")[:-1])
-    data = {} 
+    data =  []
+    label = []
     labels = {}
     with open(filepath) as fi:
         lines = fi.readlines()
@@ -19,17 +20,25 @@ def import_images(filepath, dim):
         tokens = line.split(",")
         wine = ", ".join(tokens[2:4])
         _id, imagefile = int(tokens[0]), tokens[-1].strip().replace("\n", "")
-        if _id not in data:
-            data[_id] = []
+        if _id not in labels:
+            if len(data) > 0:
+                file_x = "%s/%d_x.npy" % (out_dir, label[-1])
+                file_y = "%s/%d_y.npy" % (out_dir, label[-1])
+                np_save2(data, label, file_x, file_y)
+            data = []
+            label = []
             count = 0
-        if count > 29: 
-            continue 
         img = Image.open(os.path.join(dir_path, imagefile))
-        data[_id].append(to_np(img, dim))
+        data.append(to_np(img, dim))
+        label.append(_id)
         labels[_id] = wine
         img.close()
         count += 1
-    return data, labels
+    if len(data) > 0:
+        file_x = "%s/%d_x.npy" % (out_dir, label[-1])
+        file_y = "%s/%d_y.npy" % (out_dir, label[-1])
+        np_save2(data, label, file_x, file_y)
+    return labels
 
 def create_dataset(data_dict, batch, epoch):
     dataset = {"train":{"x":[], "y":[]}, "validation":{"x":[], "y":[]}}
@@ -62,6 +71,12 @@ def to_np(image, dim):
     image_data = np.array(np.reshape(image_data, (dim[0], dim[1], 3)))
     return image_data
 
+def np_save2(x, y, out_file_x, out_file_y):
+    #x = np.array(x)
+    y = np.array(y)
+    np.save(out_file_x, x)
+    np.save(out_file_y, y)
+
 def np_save(x, y, dir_path):
     #x = np.array(x)
     y = np.array(y)
@@ -80,13 +95,13 @@ def main():
     out_dir = sys.argv[2]
     batch = int(sys.argv[3])
     epoch = int(sys.argv[4])
-    data_dict, label_dict = import_images(in_filepath, DIMENSION)
+    label_dict = import_images(in_filepath, DIMENSION, out_dir)
     pickle.dump(label_dict, open("%s/label_dict.pkl" % (out_dir), 'wb'))
-    del label_dict
-    dataset = create_dataset(data_dict, batch, epoch)
-    del data_dict
-    np_save(dataset["train"]['x'], dataset["train"]['y'], "%s/train" % (out_dir))
-    np_save(dataset["validation"]['x'], dataset["validation"]['y'], "%s/validation" % (out_dir))
+    #del label_dict
+    #dataset = create_dataset(data_dict, batch, epoch)
+    #del data_dict
+    #np_save(dataset["train"]['x'], dataset["train"]['y'], "%s/train" % (out_dir))
+    #np_save(dataset["validation"]['x'], dataset["validation"]['y'], "%s/validation" % (out_dir))
     
 if __name__ == '__main__':
     main()
