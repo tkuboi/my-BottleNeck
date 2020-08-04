ba6cf175b8a4a946529db64860b93c3a93cdda8c
Toshihiro Kuboi <toshikuboi@hotmail.com>
2020-08-04 12:24:13 -0700

    change the text in the upload page

--- app/main/views.py
+++ app/main/views.py
@@ -28,7 +28,7 @@ def upload_file():
     return '''
     <!doctype html>
     <title>Upload new File</title>
-    <h1>Upload new File</h1>
+    <h1>Upload new image file</h1>
     <form method=post enctype=multipart/form-data>
       <input type=file name=file>
       <input type=submit value=Upload>
